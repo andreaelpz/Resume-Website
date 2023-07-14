6f701ebfd486098cc81ab065b902110faead69de
Andrea Lopez <andreael@bu.edu>
2023-07-14 15:03:09 -0500

    overall view of website done

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-function openPopup() {
-    var button = document.querySelector("button");
-    var popupContainer = document.getElementById("popupContainer");
-    var popupContent = document.getElementById("popupContent");
+// function openPopup() {
+//     var button = document.querySelector("button");
+//     var popupContainer = document.getElementById("popupContainer");
+//     var popupContent = document.getElementById("popupContent");
   
-    var buttonPosition = button.getBoundingClientRect();
-    var buttonCenterX = buttonPosition.left + button.offsetWidth / 2;
-    var buttonCenterY = buttonPosition.top + button.offsetHeight / 2;
+//     var buttonPosition = button.getBoundingClientRect();
+//     var buttonCenterX = buttonPosition.left + button.offsetWidth / 2;
+//     var buttonCenterY = buttonPosition.top + button.offsetHeight / 2;
   
-    var popupWidth = 400;
-    var popupHeight = 200;
-    var popupLeft = buttonCenterX - popupWidth / 2;
-    var popupTop = buttonCenterY - popupHeight / 2;
+//     var popupWidth = 400;
+//     var popupHeight = 200;
+//     var popupLeft = buttonCenterX - popupWidth / 2;
+//     var popupTop = buttonCenterY - popupHeight / 2;
   
-    popupContainer.style.width = "100%";
-    popupContainer.style.height = "100%";
+//     popupContainer.style.width = "100%";
+//     popupContainer.style.height = "100%";
   
-    popupContent.style.width = popupWidth + "px";
-    popupContent.style.height = popupHeight + "px";
-    popupContent.style.left = popupLeft + "px";
-    popupContent.style.top = popupTop + "px";
-  }
-  
+//     popupContent.style.width = popupWidth + "px";
+//     popupContent.style.height = popupHeight + "px";
+//     popupContent.style.left = popupLeft + "px";
+//     popupContent.style.top = popupTop + "px";
+//   }
+
+function openAboutPopup() {
+  window.location.href = "about.html";
+}
--- views.py
+++ views.py
@@ -5,3 +5,19 @@ views = Blueprint(__name__, "views")
 @views.route("/")
 def home():
     return render_template("index.html")
+
+@views.route("/home")
+def desktop():
+    return render_template("desktop.html")
+
+@views.route("/about")
+def desktop():
+    return render_template("about.html")
+
+@views.route("/experience")
+def desktop():
+    return render_template("experience.html")
+
+@views.route("/resume")
+def desktop():
+    return render_template("resume.html")
